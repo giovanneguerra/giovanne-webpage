6cb4323f707228d31c3fac92aac71931e7a22932
Giovanne Guerra <Giovanne Guerra>
2017-03-20 23:28:47 -0300

    Scripts done

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,13 @@ var gulp            =   require('gulp'),
     cssNano         =   require('gulp-cssnano'),
     uglify          =   require('gulp-uglify'),
     rename          =   require('gulp-rename'),
+    babel           =   require('gulp-babel'),
+    uglify          =   require('gulp-uglify'),
+    iife            =   require('gulp-iife'),
     browserSync     =   require('browser-sync');
 //Paths
-var sassPath = './assets/sass/**/*scss';
+var SASS_PATH       =   './assets/sass/**/*.scss';
+var SCRIPTS_PATH    =   './assets/scripts/**/*.js';
 
 gulp.task('browser-sync', function() {
   browserSync({
@@ -26,26 +30,49 @@ gulp.task('bs-reload', function () {
 
 gulp.task('sass', function(){
     gutil.log(gutil.colors.bgMagenta.white('Sass has Started'));
-    return gulp.src(sassPath)
+    return gulp.src(SASS_PATH)
         .pipe(plumber({
             errorHandler: function(error){
-                gutil.log(gutil.colors.red(error.message));
+                gutil.log(gutil.colors.magenta(error.message));
                 this.emit('end');
             }
         }))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer('last 2 versions'))
+        .pipe(autoprefixer('last 2 versions'))        
         .pipe(gulp.dest('./assets/stylesheets/'))
         .pipe(concat('main.css'))
         .pipe(cssNano())
         .pipe(rename({suffix:'.min'}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/stylesheets/'))
-        .pipe(browserSync.reload({stream:true}));;
+        .pipe(browserSync.reload({stream:true}));
 });
 
+gulp.task('scripts', function(){
+    gutil.log(gutil.colors.bgYellow.white('Script has started'));
+    return gulp.src(SCRIPTS_PATH)
+        .pipe(plumber({
+            errorHandler: function(error){
+                gutil.log(gutil.colors.yellow(error.message));
+                this.emit('end');
+            }
+        }))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(iife())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./public/scripts/'))
+        .pipe(browserSync.reload({stream: true}));
+})
+
 gulp.task('default', ['browser-sync'], function(){
-    gulp.watch(sassPath, ['sass']);
+    gulp.watch(SASS_PATH, ['sass']);
+    gulp.watch(SCRIPTS_PATH, ['scripts']);
     gulp.watch("*.html", ['bs-reload']);
 });
