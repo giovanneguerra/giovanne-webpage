f03938571b5a622d25cca9bcbc7aec72a62ef844
Giovanne Guerra <giovanneguerra@hotmail.com>
2017-03-27 22:36:15 -0300

    Images almost done and clean done with some issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+//Variables
 var gulp            =   require('gulp'),
     gutil           =   require('gulp-util'),
     sass            =   require('gulp-sass'),
@@ -11,11 +12,20 @@ var gulp            =   require('gulp'),
     babel           =   require('gulp-babel'),
     uglify          =   require('gulp-uglify'),
     iife            =   require('gulp-iife'),
+    clean           =   require('gulp-clean'),
+    imagemin        =   require('gulp-imagemin'),
     browserSync     =   require('browser-sync');
+
 //Paths
 var SASS_PATH       =   './assets/sass/**/*.scss';
 var SCRIPTS_PATH    =   './assets/scripts/**/*.js';
+var IMAGES_PATH     =   './assets/images/*.+(png|jpg|jpeg|gif|svg)';
+var SASS_DEST       =   './public/stylesheets/';
+var SCRIPTS_DEST    =   './public/scripts/';
+var IMAGES_DEST     =   './public/images/';
 
+//Tasks
+//browser-sync
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
@@ -24,10 +34,19 @@ gulp.task('browser-sync', function() {
   });
 });
 
+//Reload
 gulp.task('bs-reload', function () {
     browserSync.reload();
   });
 
+//Clean
+gulp.task('clean', function(){
+    gutil.log(gutil.colors.bgBlue.white('Cleaning the public paths'));
+    return gulp.src([SASS_DEST,SCRIPTS_DEST,IMAGES_DEST])
+        .pipe(clean({force: true}))
+});
+
+//Sass
 gulp.task('sass', function(){
     gutil.log(gutil.colors.bgMagenta.white('Sass has Started'));
     return gulp.src(SASS_PATH)
@@ -39,18 +58,19 @@ gulp.task('sass', function(){
         }))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer('last 2 versions'))        
+        .pipe(autoprefixer('last 2 versions'))
         .pipe(gulp.dest('./assets/stylesheets/'))
         .pipe(concat('main.css'))
         .pipe(cssNano())
         .pipe(rename({suffix:'.min'}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/stylesheets/'))
+        .pipe(gulp.dest(SASS_DEST))
         .pipe(browserSync.reload({stream:true}));
 });
 
+//JS
 gulp.task('scripts', function(){
-    gutil.log(gutil.colors.bgYellow.white('Script has started'));
+    gutil.log(gutil.colors.bgYellow.white('Scripts has started'));
     return gulp.src(SCRIPTS_PATH)
         .pipe(plumber({
             errorHandler: function(error){
@@ -62,17 +82,41 @@ gulp.task('scripts', function(){
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(iife())
         .pipe(concat('main.js'))
+        .pipe(iife())
         .pipe(uglify())
         .pipe(rename({suffix:'.min'}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/scripts/'))
+        .pipe(gulp.dest(SCRIPTS_DEST))
         .pipe(browserSync.reload({stream: true}));
-})
+});
 
-gulp.task('default', ['browser-sync'], function(){
+//Images
+gulp.task('images', function(){
+  gutil.log(gutil.colors.bgGreen.white('Images has started'));
+  return gulp.src(IMAGES_PATH)
+  .pipe(plumber({
+      errorHandler: function(error){
+          gutil.log(gutil.colors.green(error.message));
+          this.emit('end');
+      }
+  }))
+  .pipe(imagemin([
+  	imagemin.gifsicle({interlaced: true}),
+  	imagemin.jpegtran({progressive: true}),
+  	imagemin.optipng({optimizationLevel: 5})
+  ],
+  {
+    verbose: true
+  }))
+  .pipe(gulp.dest(IMAGES_DEST))
+  .pipe(browserSync.reload({stream:true}));
+});
+
+//Gulp
+gulp.task('default', ['clean','sass','scripts','images','browser-sync'], function(){
     gulp.watch(SASS_PATH, ['sass']);
     gulp.watch(SCRIPTS_PATH, ['scripts']);
+    gulp.watch(IMAGES_PATH, ['images']);
     gulp.watch("*.html", ['bs-reload']);
 });
